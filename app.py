188e2c61108e7a88673490a87fe89875a421eb67
Estefania Ascencio Medina <133863317+AscencioEstefania@users.noreply.github.com>
2025-11-17 14:57:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import streamlit as st
 
-# Main title
+st.set_page_config(page_title="N. incerta demo", page_icon="🧪")
+
 st.title("My first Streamlit app 🎈")
 
-# Text
 st.write("Hello, I'm Estefanía and this is my first app deployed with Streamlit and GitHub.")
 
-# Example slider
 value = st.slider("Select a number", 0, 100, 50)
 st.write("The selected value is:", value)
 
-# Text input
 name = st.text_input("Write your name:")
 if name:
     st.write(f"Hello, {name}! 👋")
